d877510a7eb41f1033734dd0ceb9f62a5ef06414
Nicolas Sigal <nicolas.sigal@globallogic.com>
2016-08-03 12:38:28 -0300

    fixed styling

--- app/routes/login/login.controller.js
+++ app/routes/login/login.controller.js
@@ -1,13 +1,16 @@
 function LoginController(){
-var self = this;
-self.error = 'Usuario/Contraseña invalido';
-  let route = {
-  	get: function(urlParams) {
+	var self = this;
+	self.error = 'Usuario / Contraseña invalido';
+	self.clickme = function (){
+		console.log("loggggin");
+	}
+	let route = {
+		get: function(urlParams) {
 			return { login: self };
-  	}
-  };
+		}
+	};
 
-  return route;
+	return route;
 }
 
 module.exports = LoginController()
