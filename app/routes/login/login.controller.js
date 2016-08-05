ad2653327b580cc3b1f372850410cd80732dfdd0
Nicolas Sigal <nicolas.sigal@globallogic.com>
2016-08-05 10:46:33 -0300

    login validate and promise

--- app/routes/login/login.controller.js
+++ app/routes/login/login.controller.js
@@ -2,37 +2,62 @@
 
 function LoginController(urlParams){
 
-	var exampleService = require("./../../services/exampleService")();
+	var authenticationService = require("./../../services/authenticationService")();
 	
 	let self = this;
 	
 	//Public methods and attributes
 	Object.assign(self, {
 		link: 	link,
-		init: 	init
+		init: 	init,
+		error: ''
 	});
 
 	return self;	
 
 	function logIn(){
-		event.preventDefault();
-		var formData = $("form").serialize();
-		$('form').validate({
+		let form = $('form');
+		$('#loginError').html('');
+		event.preventDefault();		
+		let formData  =  ConvertFormToJSON(form);
+		form.validate({
+			errorClass: "error text-danger",
+			errorElement: 'span',
+			wrapper: 'p',
 			rules:{
-				username:{
-					required:true
-				},
-				password:{
-					required:true
-				}
+				username: "required",
+				password: "required"				
+			},
+			messages:{
+				username: "Debe ingresar un usuario.",
+				password: "Debe ingresar una contraseña."
+			},
+			submitHandler: function() {
+				$.when(authenticationService.logIn(formData))
+				.then(function success(response){
+						console.log(response);
+					}, 	function error(error){
+						console.log(error);
+						$('#loginError').html(error);
+				});
 			}
-		});	
-		exampleService.showExample("example text");
+		});			
 	}	
+
+	//Convert formdata to json
+	function ConvertFormToJSON(form){
+		var array = jQuery(form).serializeArray();
+		var json = {};
+		jQuery.each(array, function() {
+			json[this.name] = this.value || '';
+		});
+		return json;
+	}
 
 	// To bind elements
 	function link(){
 		$('form').on("submit", logIn);
+		self.error = "";
 	};
 
 	// To make calls to apis. It may returns a promise.
