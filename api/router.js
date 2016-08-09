608df3373597cea9433b0299571e6cd9e392738c
Gerardo Valiani <gerardovaliani@gmail.com>
2016-08-09 14:11:03 -0300

    If user login succesfuly server set a cookie.

--- api/router.js
+++ api/router.js
@@ -1,25 +1,45 @@
 var url = require('url');
 var qs  = require('querystring');
+var Cookies = require( "cookies" );
 
 module.exports = [
-	function(req, res, next){
-	  if (req.method == 'POST') {
-	    var body = '';
-	    req.on('data', function (data) {
-	        body += data;
-	        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-	        if (body.length > 1e6) { 
-	          // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
-	          req.connection.destroy();
-	        }
-	    });
-	    req.on('end', function () {
-	      req.body = qs.parse(body);
-	      next();
-	    });
-	  } else {
-	  	next();
-	  }
+	{
+		route: '/api',
+		handle: function(req, res, next){
+			var cookies = new Cookies(req, res);
+
+			req.cookies = {
+				get: cookies.get.bind(cookies)
+			};
+
+			res.cookies = {
+				set: cookies.set.bind(cookies)
+			};
+
+			next();
+		}
+	},
+	{
+		route: '/api',
+		handle: function(req, res, next){
+		  if (req.method == 'POST') {
+		    var body = '';
+		    req.on('data', function (data) {
+		        body += data;
+		        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+		        if (body.length > 1e6) { 
+		          // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+		          req.connection.destroy();
+		        }
+		    });
+		    req.on('end', function () {
+		      req.body = qs.parse(body);
+		      next();
+		    });
+		  } else {
+		  	next();
+		  }
+		}
 	},
   {
     route: '/api',
