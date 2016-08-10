3a6316a17818a14a75b99ba945d0fc9a69c66b49
Gerardo Valiani <gerardovaliani@gmail.com>
2016-08-10 15:46:06 -0300

    Implementing idiomatic for eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,9 @@
 module.exports = {
+  "extends": "idiomatic",
   "env": {
     "browser": true,
     "commonjs": true,
     "node": true
-  },
-  "extends": "eslint:recommended",
-  "rules": {
-    "indent": ["error", "tab"],
-    "linebreak-style": ["error", "windows"],
-    "quotes": ["error", "single"],
-    "semi": ["error", "always"],
-    'no-multi-spaces': ['error', {
-      'exceptions': {
-        'Property': true,
-        'VariableDeclarator': true
-      }
-    }],
-    'key-spacing': ['error', {
-      'beforeColon': false,
-      'afterColon': true
-    }]
   },
   "globals": {
     'jQuery': true,
