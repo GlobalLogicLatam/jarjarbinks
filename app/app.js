6c05f17825892f6bdaeed7e3b0353ec70e293aa5
Gerardo Valiani <gerardovaliani@gmail.com>
2016-08-10 15:46:19 -0300

    Fixing lint errors.

--- app/app.js
+++ app/app.js
@@ -1,31 +1,33 @@
-var router = require('./app.router');
-require('script!jquery');
-require('script!jquery_validation');
-require('script!validation_additional_methods');
-require('script!sammy');
-require('script!mustache');
-require('./components/serializeObject/serializeObject')(); //convert data into json
-require('./components/sammyFormIgnore/formIgnore')(); //Sammy form submit ignore
+var router = require( './app.router' );
+require( 'script!jquery' );
+require( 'script!jquery_validation' );
+require( 'script!validation_additional_methods' );
+require( 'script!sammy' );
+require( 'script!mustache' );
+// Convert data into json
+require( './components/serializeObject/serializeObject' )();
+// Sammy form submit ignore
+require( './components/sammyFormIgnore/formIgnore' )();
 
 function App() {
-	var sammy = Sammy('#content-wrapper', function() {
-		var nav_el = $('.js-nav');
+  var sammy = Sammy( '#content-wrapper', function appSammyHandler() {
+    var nav_el = $( '.js-nav' );
 
-		// Changes element wrapper to avoid show nav element when user is on login view.
-		this.around(function(cb){
-			if(this.path == '/#/login'){
-				nav_el.hide();
-			} else {
-				nav_el.show();
-			}
-			cb();
-		});
+    // Changes element wrapper to avoid show nav element when user is on login view.
+    this.around( function checkIfLogin( cb ) {
+      if ( this.path == '/#/login' ) {
+        nav_el.hide();
+      } else {
+        nav_el.show();
+      }
+      cb();
+    } );
 
-		// Set routes
-		router(this);
-	});
+    // Set routes
+    router( this );
+  } );
 
-	return sammy;
+  return sammy;
 }
 
 module.exports = App;
