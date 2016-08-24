df9e6018ba8d19b57724d29c43388c8a3a983e74
Gerardo Valiani <gerardovaliani@gmail.com>
2016-08-24 20:40:33 -0300

    Pre-commit config and classes support for es-lint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,16 @@ module.exports = {
     '$': true,
     'Sammy': true,
     'Mustache': true,
+  },
+  'plugins': [
+    'classes'
+  ],
+  'rules': {
+    // Plugins
+    'classes/space': 2,
+    'classes/name': [2, "class", "method"],
+    'classes/constructor': 2,
+    'classes/super': 2,
+    'classes/style': 2
   }
 };
