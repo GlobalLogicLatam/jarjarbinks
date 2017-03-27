9e5512eebbe546ecfea890e895d2d64cef7eac06
Gerardo Valiani <gerardovaliani@gmail.com>
2017-03-27 17:52:20 -0300

    Showing device details in Desktop version.

--- app/desktop/routes/device/device.controller.js
+++ app/desktop/routes/device/device.controller.js
@@ -1,5 +1,7 @@
 let require_factory = require( 'modules/require-factory' ),
-  deviceService = require_factory( 'modules/services/device-service' );
+  deviceService = require_factory( 'modules/services/device-service' ),
+  detailServices = require_factory( 'modules/services/detail-service' ),
+  details_tmpl = require_factory( 'routes/device/device_details.template.mustache' );
 
 function DeviceController() {
   let self = {};
@@ -12,18 +14,31 @@ function DeviceController() {
     unlocked: 'Desbloqueado',
     locked: 'Bloqueado'
   } );
+
   return self;
 
 	// //PUBLIC FUNCTIONS
 	// To bind elements
   function link() {
 
+    $( '.js-card-detail' )
+      .on( 'click', function redirect() {
+        let device_id = this.getAttribute( 'data-detail-id' );
+
+        detailServices
+          .get( { id: device_id } )
+          .then( function( device_detail ){
+            let rendered_html = $( Mustache.render( details_tmpl, { title: device_detail[3].data, detail: device_detail } ) );
+            $('.js-device-detail').html( rendered_html );
+          } )
+      } );
+
   }
 
   // To make calls to apis. It may returns a promise.
-  function init() {
+  function init( sammyContext ) {
     // Init nav bar
-    require_factory( 'components/nav-bar/nav-bar' )();
+    require_factory( 'components/nav-bar/nav-bar' )( sammyContext );
 
     return deviceService
       .getDevicesByOS()
